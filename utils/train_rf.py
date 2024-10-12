14db7712a83089d1559c35bf075a3f7036005770
KimNganNguyen <nkngan2731@gmail.com>
2024-10-12 21:24:26 +0700

    Update train_rf.py

--- utils/train_rf.py
+++ utils/train_rf.py
@@ -1,11 +1,37 @@
 import torch
+import torch.nn as nn
 import numpy as np
 
 from sklearn.ensemble import RandomForestClassifier
 
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-def train_rf(convNet_model, train_loader, n_estimators: int = 100):
-    conv_net = convNet_model.eval()
+def train_ConvNet(convNet_model, train_loader, num_epochs: int = 10):
+    ConvNet = convNet_model.to(DEVICE)
+    
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(ConvNet.parameters(), lr=0.0001)
+    
+    loss_result = []
+    for epoch in range(num_epochs):
+        for i, (inputs, labels) in enumerate(train_loader):
+            inputs = inputs.to(DEVICE)
+            labels = labels.to(DEVICE)
+            outputs = ConvNet(inputs)
+            loss = criterion(outputs, labels)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            
+            if i % 10 == 0:
+                print(f"Epoch: {epoch + 1}/{num_epochs}, step: {i}/{len(train_loader)}, loss: {loss.item()}")
+                loss_result.append(loss.item())
+
+    return ConvNet
+
+
+def train_rf(model, train_loader, n_estimators):
+    convNet = model.eval()
     
     features = []
     labels = []
@@ -13,7 +39,7 @@ def train_rf(convNet_model, train_loader, n_estimators: int = 100):
     # Loop over each batch to extract features
     for inputs, targets in train_loader:
         with torch.no_grad():
-            feature = conv_net(inputs).numpy()
+            feature = convNet(inputs).numpy()
             features.append(feature)
             labels.append(targets.numpy())
             
