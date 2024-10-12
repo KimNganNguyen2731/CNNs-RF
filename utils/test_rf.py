31b308a10b3f43b38fa4c45b1508781def900822
KimNganNguyen <nkngan2731@gmail.com>
2024-10-12 15:42:41 +0700

    Update test_rf.py

--- utils/test_rf.py
+++ utils/test_rf.py
@@ -2,21 +2,22 @@ import torch
 import numpy as np
 
 def predict_rf(convNet_model, rf_classifier, test_loader):
-    conv_net = convNet_model.eval()  # Đặt CNN ở chế độ đánh giá (evaluation)
+    conv_net = convNet_model.eval()  # set CNNs model to evaluation mode (evaluation)
 
     all_predictions = []
     true_labels = []
 
     # Lặp qua từng batch trong test_loader để trích xuất đặc trưng và dự đoán
+    # loop over each batch in test_loader to extract features and predict labels
     for inputs, targets in test_loader:
         with torch.no_grad():
-            # Trích xuất đặc trưng từ CNN
+            # Extract features with CNNs model
             features = conv_net(inputs).numpy()
 
-            # Dự đoán với mô hình Random Forest
+            # Predict with Random Forest
             predictions = rf_classifier.predict(features)
 
             all_predictions.extend(predictions)
-            true_labels.extend(targets.numpy())  # Lưu trữ nhãn thực tế để so sánh sau này
+            true_labels.extend(targets.numpy())  # Save the real labels to compare with the predicted labels
 
     return np.array(all_predictions), np.array(true_labels)
