c511b3c30bda97e5c18ceecd73a97e559fff9b74
KimNganNguyen2731 <nkngan2731@gmail.com>
2024-10-13 23:27:38 +0700

    Update test_rf.py

--- utils/test_rf.py
+++ utils/test_rf.py
@@ -2,6 +2,22 @@ import torch
 import numpy as np
 from tools.tools import get_fc_features
 
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def test(model, test_loader):
+  model.eval()
+  n_correct = 0
+  n_samples = 0
+  for inputs, labels in test_loader:
+    inputs = inputs.to(DEVICE)
+    labels = labels.to(DEVICE)
+    label_predicted = model(inputs)
+    _, predicted = torch.max(label_predicted, 1)
+    n_samples += labels.size(0)
+    n_correct += (predicted == labels).sum().item()
+  print(f'Accuracy of ConvNet = {100*n_correct/n_samples:.2f}')
+    
+
 def predict_rf(model, rf_classifier, test_loader):
     model.eval()  # Set CNN model to evaluation module
     all_predictions = []
