8c677a1f026fe23ccffdf5f202ae5767172e3e07
KimNganNguyen2731 <nkngan2731@gmail.com>
2024-10-13 22:06:19 +0700

    Create dataloader_CIFAR10.py

---
+++ tools/dataloader_CIFAR10.py
@@ -0,0 +1,19 @@
+# Data loading
+def dataloader():
+  train_dataset = datasets.CIFAR10(
+      root = "data",
+      train = True,
+      download=True,
+      transform = transforms.ToTensor()
+  )
+
+  test_dataset = datasets.CIFAR10(
+      root = "data",
+      train = False,
+      download = True,
+      transform = transforms.ToTensor()
+  )
+  # Data Loader
+  train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+  test_loader = DataLoader(test_dataset, batch_size=64, shuffle=True)
+  return train_loader, test_loader
